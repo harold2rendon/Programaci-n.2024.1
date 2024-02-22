a53b1f600892f0167d452abb9ae51d7428f37886
Harold Steven Rendon Urrea <harolds.rendonu@uqvirtual.edu.com>
2024-02-22 14:46:04 -0500

    cambios peludos

---
+++ veterinariapeludos/src/main/java/co/edu/uniquindio/poo/Peludos.java
@@ -0,0 +1,10 @@
+package co.edu.uniquindio.poo;
+
+public record Peludos(String nombre, String especie, String raza, byte edad, String género, String  color, byte peso ) {
+    public Peludos {
+        
+    }
+    
+    public void caminar(int metros){
+    }
+}
